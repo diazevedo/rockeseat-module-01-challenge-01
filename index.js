7d046cda3a884a619a098f28d8ee14568974fe6b
Diego Azevedo <dapptn@hotmail.com>
2019-08-21 16:59:30 +1000

    Post route created. Projects can be addded

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 
 const server = express();
+server.use(express.json());
+
+const projectList = [];
 
 server.get("/", (req, res) => {
-  res.send("Welcome");
+  res.send("Welcome to projects storage api");
+});
+
+server.post("/projects", (req, res) => {
+  const project = ({ id, title, tasks } = req.body);
+  projectList.push(project);
+  res.json(projectList);
 });
 
 server.listen(3001, () => {
